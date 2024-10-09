e37fdeb229958910ce90acdc330a315640664da9
hoycid <quilangcidrex@gmail.com>
2024-10-09 17:05:59 +1300

    added total runes to context

--- src/Providers/LevelContext.js
+++ src/Providers/LevelContext.js
@@ -3,10 +3,13 @@ import React, { useState, createContext, useContext } from "react";
 const LevelContext = createContext();
 
 export const LevelProvider = ({ children }) => {
-  const [level, setLevel] = useState(1);
+  const [level, setLevel] = useState(0);
+  const [totalRunes, setTotalRunes] = useState(0);
 
   return (
-    <LevelContext.Provider value={{ level, setLevel }}>
+    <LevelContext.Provider
+      value={{ level, setLevel, totalRunes, setTotalRunes }}
+    >
       {children}
     </LevelContext.Provider>
   );
