9aab7de064393e26160de540b3ec3f5d390b3c2c
hoycid <quilangcidrex@gmail.com>
2024-10-09 17:55:42 +1300

    reduced repetition, imported classess file

--- src/Components/CharacterPlanner.js
+++ src/Components/CharacterPlanner.js
@@ -10,45 +10,10 @@ import { useLevel } from "../Providers/LevelContext";
 import Subinfo from "./Subinfo";
 
 import { calculateRunesToLevel } from "../Utilities/calculateBaseStats";
-
-const classes = {
-  hero: {
-    initLvl: 7,
-    vig: 14,
-    mind: 9,
-    end: 12,
-    str: 16,
-    dex: 9,
-    int: 7,
-    faith: 8,
-    arc: 11,
-  },
-  bandit: {
-    initLvl: 5,
-    vig: 10,
-    mind: 11,
-    end: 10,
-    str: 9,
-    dex: 13,
-    int: 9,
-    faith: 8,
-    arc: 14,
-  },
-  astrologer: {
-    initLvl: 6,
-    vig: 9,
-    mind: 15,
-    end: 9,
-    str: 8,
-    dex: 12,
-    int: 16,
-    faith: 7,
-    arc: 9,
-  },
-};
+import CLASSES from "../Utilities/CLASSES";
 
 const CharacterPlanner = () => {
-  const [selectedClass, setSelectedClass] = useState(classes.hero);
+  const [selectedClass, setSelectedClass] = useState(CLASSES.hero);
   const [baseStats, setBaseStats] = useState(selectedClass);
   const [currentStats, setCurrentStats] = useState(selectedClass);
   const [calculatedStats, setCalculatedStats] = useState({
@@ -63,8 +28,8 @@ const CharacterPlanner = () => {
   }, [selectedClass]);
 
   const onSelectClass = option => {
-    setSelectedClass(classes[option]);
-    setCurrentStats(classes[option]);
+    setSelectedClass(CLASSES[option]);
+    setCurrentStats(CLASSES[option]);
   };
 
   const onAlterStat = (stat, statVal) => {
@@ -72,7 +37,6 @@ const CharacterPlanner = () => {
       ...selectedClass,
       [stat]: statVal,
     };
-
     setCurrentStats(updatedStats);
   };
 
@@ -83,7 +47,6 @@ const CharacterPlanner = () => {
     });
 
     let runes = 0;
-
     for (let i = level - 1; i >= selectedClass.initLvl; i--) {
       runes = runes + calculateRunesToLevel(i);
     }
@@ -92,69 +55,72 @@ const CharacterPlanner = () => {
   };
 
   return (
-    <div className="App">
+    <>
       <Panel title="Base Stats">
         <h3>Level {level}</h3>
         <Dropdown
           name="Class"
-          classes={classes}
+          classes={CLASSES}
           onSelectClass={onSelectClass}
         />
-        <Counter name="vig" count={baseStats.vig} onAlterStat={onAlterStat} />
-        <Counter name="mind" count={baseStats.mind} onAlterStat={onAlterStat} />
-        <Counter name="end" count={baseStats.end} onAlterStat={onAlterStat} />
-        <Counter name="str" count={baseStats.str} onAlterStat={onAlterStat} />
-        <Counter name="dex" count={baseStats.dex} onAlterStat={onAlterStat} />
-        <Counter name="int" count={baseStats.int} onAlterStat={onAlterStat} />
-        <Counter
-          name="faith"
-          count={baseStats.faith}
-          onAlterStat={onAlterStat}
-        />
-        <Counter name="arc" count={baseStats.arc} onAlterStat={onAlterStat} />
-        <Panel>
-          <Info
-            name="runesToLevel"
-            stat={level}
-            onCalculateStat={onCalculateStat}
-          />
-          <Info name="totalRunes" stat={totalRunes} />
-        </Panel>
       </Panel>
       <Panel>
-        <Info
-          name="hp"
-          stat={currentStats.vig}
-          onCalculateStat={onCalculateStat}
-        />
-        <Info
-          name="fp"
-          stat={currentStats.mind}
-          onCalculateStat={onCalculateStat}
-        />
-        <Info
-          name="stamina"
-          stat={currentStats.end}
-          onCalculateStat={onCalculateStat}
-        />
-        <Info
-          name="equipLoad"
-          stat={currentStats.end}
-          onCalculateStat={onCalculateStat}
-        />
-        <Subinfo name="weightStatus" stat={calculatedStats.equipload} />
+        {Object.entries(currentStats)
+          .filter(([name]) => name !== "initLvl")
+          .map(([name, value]) => (
+            <Info key={name} name={name} stat={value} />
+          ))}
       </Panel>
-      <Info
-        name="poise"
-        stat={calculatedStats.totalWeight}
-        onCalculateStat={onCalculateStat}
-      />
-      <Info
-        name="discovery"
-        stat={currentStats.arc}
-        onCalculateStat={onCalculateStat}
-      />
-    </div>
+      <Panel>
+        {Object.entries(baseStats)
+          .filter(([name]) => name !== "initLvl")
+          .map(([name, value]) => (
+            <Counter
+              key={name}
+              name={name}
+              count={value}
+              onAlterStat={onAlterStat}
+            />
+          ))}
+      </Panel>
+
+      <Panel>
+        <Info
+          name="runesToLevel"
+          stat={level}
+          onCalculateStat={onCalculateStat}
+        />
+        <Info name="totalRunes" stat={totalRunes} />
+      </Panel>
+
+      <Panel>
+        {[
+          { name: "hp", stat: currentStats.vig },
+          { name: "fp", stat: currentStats.mind },
+          { name: "stamina", stat: currentStats.end },
+          { name: "equipLoad", stat: currentStats.end },
+        ].map(({ name, stat }) => (
+          <Info
+            key={name}
+            name={name}
+            stat={stat}
+            onCalculateStat={onCalculateStat}
+          />
+        ))}
+        <Subinfo name="weightStatus" stat={calculatedStats.equipload} />
+
+        <Info
+          name="poise"
+          stat={calculatedStats.totalWeight}
+          onCalculateStat={onCalculateStat}
+        />
+        <Info
+          name="discovery"
+          stat={currentStats.arc}
+          onCalculateStat={onCalculateStat}
+        />
+      </Panel>
+    </>
   );
 };
 
