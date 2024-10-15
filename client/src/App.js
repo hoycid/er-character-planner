1574b26b4fb0d468d18d8a74b34c40f4e6d802f8
hoycid <quilangcidrex@gmail.com>
2024-10-15 14:04:41 +1300

    fetch classes from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react";
 import { LevelProvider } from "./providers/LevelProvider";
 import CharacterPlanner from "./pages/CharacterPlanner";
 
 function App() {
+  const [charClasses, setCharClasses] = useState(undefined);
+
+  useEffect(() => {
+    fetch("/classes")
+      .then(response => response.json())
+      .then(data => {
+        setCharClasses(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <LevelProvider>
-        <CharacterPlanner />
-      </LevelProvider>
+      {typeof charClasses === "undefined" ? (
+        <p>Loading</p>
+      ) : (
+        <LevelProvider>
+          <CharacterPlanner classes={charClasses} />
+        </LevelProvider>
+      )}
     </div>
   );
 }
