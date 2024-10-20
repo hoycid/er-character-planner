f62300dbec28e5c99050337183c132793d41e611
hoycid <quilangcidrex@gmail.com>
2024-10-20 20:57:01 +1300

    removed proxy for testing

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,13 @@ function App() {
   const [characters, setCharacters] = useState(undefined);
 
   useEffect(() => {
-    fetch("/classes")
+    fetch("https://er-character-planner-production.up.railway.app/classes")
       .then(response => response.json())
       .then(data => {
         setCharClasses(data);
       });
 
-    fetch("/characters")
+    fetch("https://er-character-planner-production.up.railway.app/characters")
       .then(response => response.json())
       .then(data => {
         setCharacters(data.characters);
