2e965e5f4b63e7e6cbdc2cbcc21f70a6f3dab5cf
hoycid <quilangcidrex@gmail.com>
2024-10-23 18:25:00 +1300

    added fetch for armors from elden ring database api, served armors to app

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import CharacterPlanner from "./pages/CharacterPlanner";
 function App() {
   const [charClasses, setCharClasses] = useState(undefined);
   const [characters, setCharacters] = useState(undefined);
+  const [armors, setArmors] = useState(undefined);
 
   const handleSaveCharacter = char => {
     setCharacters(prevCharacters => [...prevCharacters, char]);
@@ -50,22 +51,51 @@ function App() {
     }
   };
 
-  // Call the fetch functions as needed (e.g., in useEffect)
+  const fetchArmors = async () => {
+    let allArmors = []; // Initialize an array to store all armors
+    const totalPages = 6; // Define the total number of pages you want to fetch (0 to 5)
+
+    try {
+      // Loop through each page
+      for (let page = 0; page < totalPages; page++) {
+        const response = await fetch(
+          `https://eldenring.fanapis.com/api/armors?limit=100&page=${page}`
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Error fetching armors page ${page}: ${response.statusText}`
+          );
+        }
+
+        const data = await response.json();
+        allArmors = [...allArmors, ...data.data]; // Add fetched data to allArmors
+      }
+
+      setArmors(allArmors); // Final state update after fetching all pages
+    } catch (error) {
+      console.error("Error fetching armors:", error);
+    }
+  };
+
   useEffect(() => {
+    fetchArmors();
     fetchCharClasses();
     fetchCharacters();
-  }, [characters]);
+  }, []);
 
   return (
     <div className="App">
-      {typeof charClasses === "undefined" &&
-      typeof characters === "undefined" ? (
+      {typeof charClasses === "undefined" ||
+      typeof characters === "undefined" ||
+      typeof armors === "undefined" ? (
         <p>Loading...</p>
       ) : (
         <LevelProvider>
           <CharacterPlanner
             classes={charClasses}
             characters={characters}
+            armors={armors}
             onSaveCharacter={handleSaveCharacter}
             onDeleteCharacter={handleDeleteCharacter}
           />
